f9feb4e458ce779de10b7fe0384a1b88ffed5f85
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-05-26 02:45:50 -0600

    Update: 00.Introduction.py 00.IntroductionII.py ARM Template.json README.md notebooks

--- 00.IntroductionII.py
+++ 00.IntroductionII.py
@@ -5,10 +5,7 @@ from pyspark.sql.types import *
 # COMMAND ----------
 
 # Create the Departments
-department1 = Row(id='123456', name='Computer Science')
-department2 = Row(id='789012', name='Mechanical Engineering')
-department3 = Row(id='345678', name='Theater and Drama')
-department4 = Row(id='901234', name='Indoor Recreation')
+department1= row(id='123456', name='Computer Science')
 
 # COMMAND ----------
 
