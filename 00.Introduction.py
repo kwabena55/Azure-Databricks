ea4b454e205bb4db59de759a28e66d962a03bf1b
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-05-26 02:56:05 -0600

    Add Files

--- 00.Introduction.py
+++ 00.Introduction.py
@@ -13,3 +13,29 @@ from pyspark.sql.types import st
 
 # COMMAND ----------
 
+df1 = spark.read.format("csv").load("dbfs:/FileStore/shared_uploads/peniel551@outlook.com/airports.csv",header=True)
+
+# COMMAND ----------
+
+df1.take(5)
+
+# COMMAND ----------
+
+df1.show()
+
+# COMMAND ----------
+
+df1.createOrReplaceTempView('SQL')
+
+# COMMAND ----------
+
+
+spark.sql ("Select * from sql").show()
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from df1
+
+# COMMAND ----------
+
