c6269b76b75926f6e59a4c659c083f0352ad510d
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-05-26 02:30:16 -0600

    Saving all files to Github

---
+++ notebooks/Users/peniel551@outlook.com/Solomon Databricks Project/00.Introduction.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC select current_timestamp
+
+# COMMAND ----------
+
+from databricks import koalas as ks
+
+# COMMAND ----------
+
+from pyspark.sql.functions import format_number
+from pyspark.sql.types import StringType,StructType
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
