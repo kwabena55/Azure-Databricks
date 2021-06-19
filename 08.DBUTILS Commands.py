6cd329f3937b2e6959b94805b9f9e1232272a82c
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-06-19 13:47:23 -0600

    Update: 00.Introduction.py 00.IntroductionII.py 02.Databases and Tables.py 03.Load Files in Databricks.py 04.Structured Streaming using Python DataFrames.py 05.2021-06-19 - JDBC Example.py 06. Data Managements Using Scala.scala 07.SQL Notebook.sql 08.DBUTILS Commands.py ARM Template.json Connect by JDBC.py README.md

--- 08.DBUTILS Commands.py
+++ 08.DBUTILS Commands.py
@@ -1,1 +1,19 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # DBUTILS Python
+
+# COMMAND ----------
+
+# grab all dbutils
+dbutils.help()
+
+# COMMAND ----------
+
+dbutils.fs.help()
+
+# COMMAND ----------
+
+dbutils.secrets.help()
+
+# COMMAND ----------
+
