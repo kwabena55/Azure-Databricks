56a15c84bc696fbc6dc1724d7d27ef26f68a730f
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-06-19 14:25:09 -0600

    Update: 00.Introduction.py 00.IntroductionII.py 02.Databases and Tables.py 03.Load Files in Databricks.py 04.Structured Streaming using Python DataFrames.py 05.2021-06-19 - JDBC Example.py 06. Data Managements Using Scala.scala 07.SQL Notebook.sql 08.DBUTILS Commands.py ARM Template.json Connect by JDBC.py README.md

--- 08.DBUTILS Commands.py
+++ 08.DBUTILS Commands.py
@@ -17,3 +17,60 @@ dbutils.secrets.help()
 
 # COMMAND ----------
 
+dbutils.secrets.listScopes()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # List files in Filepath
+
+# COMMAND ----------
+
+# list files in Filepath
+dbutils.fs.ls("/FileStore/tables")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Create Some Folders in a path
+
+# COMMAND ----------
+
+# Create Some Folders in a path
+dbutils.fs.mkdirs("/FileStore/NewFolder")
+dbutils.fs.mkdirs("/FileStore/NewFolder/demo1")
+
+# COMMAND ----------
+
+dbutils.fs.mkdirs("/FileStore/tables/demo1")
+dbutils.fs.mkdirs("/FileStore/tables/demo2")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Create some files in a Specifc Path
+
+# COMMAND ----------
+
+# create files in a Specific Path
+dbutils.fs.put("/FileStore/tables/demo1/solomon.txt","welcome to Solomon Online trainings")
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+dbutils.fs.put("/FileStore/tables/demo2/Ansah.txt","welcome to Ansah Online trainings")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Read Data stored in Files
+
+# COMMAND ----------
+
+dbutils.fs.head("/FileStore/tables/demo2/Ansah.txt")
+
+# COMMAND ----------
+
