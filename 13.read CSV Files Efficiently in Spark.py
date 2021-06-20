a9a63da6fecef967792022d129f846da250d6e0b
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-06-20 12:52:02 -0600

    Update: 00.Introduction.py 00.IntroductionII.py 02.Databases and Tables.py 03.Load Files in Databricks.py 04.Structured Streaming using Python DataFrames.py 05.2021-06-19 - JDBC Example.py 06. Data Managements Using Scala.scala 07.SQL Notebook.sql 08.DBUTILS Commands.py 09.Variables, Widgets, Notebook Parameters.py 11. Azure Databricks CLI.py 13.read CSV Files Efficiently in Spark.py ARM Template.json Connect by JDBC.py README.md

--- 13.read CSV Files Efficiently in Spark.py
+++ 13.read CSV Files Efficiently in Spark.py
@@ -1,1 +1,11 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # Create Directories First
+
+# COMMAND ----------
+
+dbutils.fs.rm("FileStore/tables/csvfile")
+dbutils.fs.rm("FileStore/tables/csvfile")
+
+# COMMAND ----------
+
