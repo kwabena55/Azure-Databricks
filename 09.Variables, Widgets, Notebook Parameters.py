6d4242218d1bd5f4c4934268a95e2045e535d14b
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-06-19 18:05:26 -0600

    Update: 00.Introduction.py 00.IntroductionII.py 02.Databases and Tables.py 03.Load Files in Databricks.py 04.Structured Streaming using Python DataFrames.py 05.2021-06-19 - JDBC Example.py 06. Data Managements Using Scala.scala 07.SQL Notebook.sql 08.DBUTILS Commands.py 09.Variables, Widgets, Notebook Parameters.py ARM Template.json Connect by JDBC.py README.md

--- 09.Variables, Widgets, Notebook Parameters.py
+++ 09.Variables, Widgets, Notebook Parameters.py
@@ -1,1 +1,33 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # Pass a single Parameter
+
+# COMMAND ----------
+
+ dbutils.widgets.text("sourcepath","")
+
+# COMMAND ----------
+
+sourcelocation=dbutils.widgets.get("sourcepath")
+sourcelocation
+
+# COMMAND ----------
+
+# MAGIC  %md
+# MAGIC  # Dropdown : Default value should be in the Choice List
+
+# COMMAND ----------
+
+dbutils.widgets.dropdown("subject_dd","payments", ["sourcepath","payments,","sales","orders","transactions"])
+
+# COMMAND ----------
+
+# Default value must be in choice list, if not use error handling methods
+try:
+  dbutils.widgets.dropdown("subject_dd","payments", ["sourcepath","payments,","sales","orders","transactions"])
+  except:
+    print("exception is raised")
+  
+
+# COMMAND ----------
+
