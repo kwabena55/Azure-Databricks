fe66f8426e17d5241489ea0154726c80fade58e7
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-06-20 13:09:24 -0600

    Update: 00.Introduction.py 00.IntroductionII.py 02.Databases and Tables.py 03.Load Files in Databricks.py 04.Structured Streaming using Python DataFrames.py 05.2021-06-19 - JDBC Example.py 06. Data Managements Using Scala.scala 07.SQL Notebook.sql 08.DBUTILS Commands.py 09.Variables, Widgets, Notebook Parameters.py 11. Azure Databricks CLI.py 13.read CSV Files Efficiently in Spark.py ARM Template.json Connect by JDBC.py README.md

--- 13.read CSV Files Efficiently in Spark.py
+++ 13.read CSV Files Efficiently in Spark.py
@@ -4,8 +4,23 @@
 
 # COMMAND ----------
 
-dbutils.fs.rm("FileStore/tables/csvfile")
-dbutils.fs.rm("FileStore/tables/csvfile")
+dbutils.fs.ls("FileStore/tables/")
+
 
 # COMMAND ----------
 
+# Create Folders in Filstore PAth
+dbutils.fs.mkdirs("FileStore/tables/csvfile/")
+dbutils.fs.mkdirs("FileStore/tables/tsvfile/")
+dbutils.fs.mkdirs("FileStore/tables/pipefile/")
+
+# COMMAND ----------
+
+# Create Folders in Filstore PAth to store multifiles
+dbutils.fs.mkdirs("FileStore/tables/multicsvfile/")
+dbutils.fs.mkdirs("FileStore/tables/multitsvfile/")
+dbutils.fs.mkdirs("FileStore/tables/multipipefile/")
+
+# COMMAND ----------
+
+ 
