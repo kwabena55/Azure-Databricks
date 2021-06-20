dba1b36b804f4faf2a1d9d8a32d6f02a4efb74a4
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-06-20 13:46:06 -0600

    Update: 00.Introduction.py 00.IntroductionII.py 02.Databases and Tables.py 03.Load Files in Databricks.py 04.Structured Streaming using Python DataFrames.py 05.2021-06-19 - JDBC Example.py 06. Data Managements Using Scala.scala 07.SQL Notebook.sql 08.DBUTILS Commands.py 09.Variables, Widgets, Notebook Parameters.py 11. Azure Databricks CLI.py 13.read CSV Files Efficiently in Spark.py 14.Read TSV Files Efficiently in Spark.py 15.Read  All PIPE Delimited CSV Files in Spark.py ARM Template.json Connect by JDBC.py README.md

--- 15.Read  All PIPE Delimited CSV Files in Spark.py
+++ 15.Read  All PIPE Delimited CSV Files in Spark.py
@@ -1,1 +1,36 @@
 # Databricks notebook source
+# Read Files in Databricks
+df=spark.read.csv("/FileStore/tables/multicsvfile/employeesus.csv", header=True, inferSchema=True,sep="|")
+
+# COMMAND ----------
+
+# Read Files in Databricks. Dont have a PIPE file so use comma separator please
+sourcepath="dbfs:/FileStore/tables/multicsvfile/employeesus.csv"
+df=spark.read.csv(sourcepath, header=True, inferSchema=True,sep=",")
+
+# COMMAND ----------
+
+df.show()
+
+# COMMAND ----------
+
+# spark=SparkSession.builder.appName("Read CSV Files").getOrCreate Use this when you wowrking local Apache spark
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+# COMMAND ----------
+
+schema_type= StructType([ StructField("FirstName",StringType(),True),
+                          StructField("Gender",StringType(),True),
+                          StructField("StartDate",DateType(),True),
+                          StructField("Salary",IntegerType(),True)]
+)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Read Multiple Files in the Source Path
+
+# COMMAND ----------
+
+sourcepath_multiple= "/FileStore/tables/multicsvfile/*"
