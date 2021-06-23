2626c19f8d5ddee9895e5d39ece5b100201b42fb
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-06-23 11:17:05 -0600

    Update: 00.Introduction.py 00.IntroductionII.py 02.Databases and Tables.py 03.Load Files in Databricks.py 04.Structured Streaming using Python DataFrames.py 05.2021-06-19 - JDBC Example.py 06. Data Managements Using Scala.scala 07.SQL Notebook.sql 08.DBUTILS Commands.py 09.Variables, Widgets, Notebook Parameters.py 11. Azure Databricks CLI.py 13.read CSV Files Efficiently in Spark.py 14.Read TSV Files Efficiently in Spark.py 15.Read  All PIPE Delimited CSV Files in Spark.py 16.Use Params and Read CSV Files with Multiple Delimiters.py 20.Read and Write Parquet  File in DB.py ARM Template.json Connect by JDBC.py README.md

--- 20.Read and Write Parquet  File in DB.py
+++ 20.Read and Write Parquet  File in DB.py
@@ -8,3 +8,24 @@ dbutils.fs.mkdirs("/FileStore/tables/Parquet")
 
 # COMMAND ----------
 
+dbutils.fs.mkdirs("/FileStore/tables/Parquet_Output")
+
+# COMMAND ----------
+
+pardf=spark.read.parquet("/FileStore/tables/Parquet/NYCTripSmall.parquet")
+display(pardf)
+
+# COMMAND ----------
+
+pardf.describe().show()
+
+# COMMAND ----------
+
+pardf.createOrReplaceTempView("sql")
+
+# COMMAND ----------
+
+spark.sql("select count(*) from sql").show()
+
+# COMMAND ----------
+
