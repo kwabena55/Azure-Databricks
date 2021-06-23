9ee2bd59849e8cc4e24e4e27f9ac3003caa7599f
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-06-23 11:31:17 -0600

    Update: 00.Introduction.py 00.IntroductionII.py 02.Databases and Tables.py 03.Load Files in Databricks.py 04.Structured Streaming using Python DataFrames.py 05.2021-06-19 - JDBC Example.py 06. Data Managements Using Scala.scala 07.SQL Notebook.sql 08.DBUTILS Commands.py 09.Variables, Widgets, Notebook Parameters.py 11. Azure Databricks CLI.py 13.read CSV Files Efficiently in Spark.py 14.Read TSV Files Efficiently in Spark.py 15.Read  All PIPE Delimited CSV Files in Spark.py 16.Use Params and Read CSV Files with Multiple Delimiters.py 20.Read and Write Parquet  File in DB.py ARM Template.json Connect by JDBC.py README.md

--- 20.Read and Write Parquet  File in DB.py
+++ 20.Read and Write Parquet  File in DB.py
@@ -54,3 +54,25 @@ pardf2.printSchema() # PrintSchema is more Powerful
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC # Write Data to an Output
+
+# COMMAND ----------
+
+output_path="/FileStore/tables/Parquet_Output"
+
+# COMMAND ----------
+
+pardf.write.mode("overwrite").parquet(output_path)
+
+# COMMAND ----------
+
+dbutils.fs.head("/FileStore/tables/Parquet_Output/part-00000-tid-52903537881445447-aed131e0-3cd2-4858-a051-2c3f0194a26a-17-1-c000.snappy.parquet")
+
+# COMMAND ----------
+
+# Reading Data
+spark.read.format("parquet").load("/FileStore/tables/Parquet_Output/part-00000-tid-52903537881445447-aed131e0-3cd2-4858-a051-2c3f0194a26a-17-1-c000.snappy.parquet").show()
+
+# COMMAND ----------
+
