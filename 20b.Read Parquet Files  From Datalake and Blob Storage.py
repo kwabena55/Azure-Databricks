b80d6043d36f8180c632339f70a35e491156dbc9
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-06-24 23:23:02 -0600

    Update: 00.Introduction.py 00.IntroductionII.py 02.Databases and Tables.py 03.Load Files in Databricks.py 04.Structured Streaming using Python DataFrames.py 05.2021-06-19 - JDBC Example.py 06. Data Managements Using Scala.scala 07.SQL Notebook.sql 08.DBUTILS Commands.py 09.Variables, Widgets, Notebook Parameters.py 11. Azure Databricks CLI.py 13.read CSV Files Efficiently in Spark.py 14.Read TSV Files Efficiently in Spark.py 15.Read  All PIPE Delimited CSV Files in Spark.py 16.Use Params and Read CSV Files with Multiple Delimiters.py 20.Read and Write Parquet  File in DB.py 20b.Read Parquet Files  From Datalake and Blob Storage.py ARM Template.json Connect by JDBC.py README.md

--- 20b.Read Parquet Files  From Datalake and Blob Storage.py
+++ 20b.Read Parquet Files  From Datalake and Blob Storage.py
@@ -1,1 +1,26 @@
 # Databricks notebook source
+# Set the Configuration
+# spark.conf.set(
+#    "fs.azure.account.key.<storage-account-name>.dfs.core.windows.net",
+#    dbutils.secrets.get(scope="<scope-name>",
+#    key="<storage-account-access-key-name>"))
+
+# COMMAND ----------
+
+spark.conf.set("fs.azure.account.key.adlstestg55.dfs.core.windows.net","969+71yPsy6O7riN0kAdTNHqWSMSRBnkLlL9oV6aPJd+btNOq4qUddrQS+4m5Id7xrzyld1P7L8K33UXxxOWqA==")
+
+# COMMAND ----------
+
+#Specify the source path
+# sourcepath= spark.read.parquet("abfss://<container-name>@<storage-account-name>.dfs.core.windows.net/<directory-name>")
+
+# COMMAND ----------
+
+sourcepath="abfss://datalakecontainer@adlstestg55.dfs.core.windows.net/parquetdata/"
+
+# COMMAND ----------
+
+df=spark.read.parquet(sourcepath)
+
+# COMMAND ----------
+
