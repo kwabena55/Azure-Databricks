a7611611d0f86b883b0aa3072ec3399eaee1c6bc
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-06-26 23:18:19 -0600

    Update: 00.Introduction.py 00.IntroductionII.py 02.Databases and Tables.py 03.Load Files in Databricks.py 04.Structured Streaming using Python DataFrames.py 05.2021-06-19 - JDBC Example.py 06. Data Managements Using Scala.scala 07.SQL Notebook.sql 08.DBUTILS Commands.py 09.Variables, Widgets, Notebook Parameters.py 11. Azure Databricks CLI.py 13.read CSV Files Efficiently in Spark.py 14.Read TSV Files Efficiently in Spark.py 15.Read  All PIPE Delimited CSV Files in Spark.py 16.Use Params and Read CSV Files with Multiple Delimiters.py 20.Read and Write Parquet  File in DB.py 20b.Read Parquet Files  From Datalake and Blob Storage.py ARM Template.json Connect by JDBC.py README.md

--- 20b.Read Parquet Files  From Datalake and Blob Storage.py
+++ 20b.Read Parquet Files  From Datalake and Blob Storage.py
@@ -1,26 +1,31 @@
-# Databricks notebook source
-# Set the Configuration
-# spark.conf.set(
-#    "fs.azure.account.key.<storage-account-name>.dfs.core.windows.net",
-#    dbutils.secrets.get(scope="<scope-name>",
-#    key="<storage-account-access-key-name>"))
+// Databricks notebook source
+// MAGIC %python
+// MAGIC # Set the Configuration
+// MAGIC # spark.conf.set(
+// MAGIC #    "fs.azure.account.key.<storage-account-name>.dfs.core.windows.net",
+// MAGIC #    dbutils.secrets.get(scope="<scope-name>",
+// MAGIC #    key="<storage-account-access-key-name>"))
 
-# COMMAND ----------
+// COMMAND ----------
 
-spark.conf.set("fs.azure.account.key.adlstestg55.dfs.core.windows.net","969+71yPsy6O7riN0kAdTNHqWSMSRBnkLlL9oV6aPJd+btNOq4qUddrQS+4m5Id7xrzyld1P7L8K33UXxxOWqA==")
+// MAGIC %python
+// MAGIC spark.conf.set("fs.azure.account.key.adlstestg55.dfs.core.windows.net","969+71yPsy6O7riN0kAdTNHqWSMSRBnkLlL9oV6aPJd+btNOq4qUddrQS+4m5Id7xrzyld1P7L8K33UXxxOWqA==")
 
-# COMMAND ----------
+// COMMAND ----------
 
-#Specify the source path
-# sourcepath= spark.read.parquet("abfss://<container-name>@<storage-account-name>.dfs.core.windows.net/<directory-name>")
+// MAGIC %python
+// MAGIC #Specify the source path
+// MAGIC # sourcepath= spark.read.parquet("abfss://<container-name>@<storage-account-name>.dfs.core.windows.net/<directory-name>")
 
-# COMMAND ----------
+// COMMAND ----------
 
-sourcepath="abfss://datalakecontainer@adlstestg55.dfs.core.windows.net/parquetdata/"
+// MAGIC %python
+// MAGIC sourcepath="abfss://datalakecontainer@adlstestg55.dfs.core.windows.net/parquetdata/"
 
-# COMMAND ----------
+// COMMAND ----------
 
-df=spark.read.parquet(sourcepath)
+// MAGIC %python
+// MAGIC df=spark.read.parquet(sourcepath)
 
-# COMMAND ----------
+// COMMAND ----------
 
