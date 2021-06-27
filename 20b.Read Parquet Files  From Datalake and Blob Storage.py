02e3011903b82de4874a880481927f329e160788
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-06-26 23:23:29 -0600

    Update: 00.Introduction.py 00.IntroductionII.py 02.Databases and Tables.py 03.Load Files in Databricks.py 04.Structured Streaming using Python DataFrames.py 05.2021-06-19 - JDBC Example.py 06. Data Managements Using Scala.scala 07.SQL Notebook.sql 08.DBUTILS Commands.py 09.Variables, Widgets, Notebook Parameters.py 11. Azure Databricks CLI.py 13.read CSV Files Efficiently in Spark.py 14.Read TSV Files Efficiently in Spark.py 15.Read  All PIPE Delimited CSV Files in Spark.py 16.Use Params and Read CSV Files with Multiple Delimiters.py 20.Read and Write Parquet  File in DB.py 20b.Read Parquet Files  From Datalake and Blob Storage.py ARM Template.json Connect by JDBC.py README.md

--- 20b.Read Parquet Files  From Datalake and Blob Storage.py
+++ 20b.Read Parquet Files  From Datalake and Blob Storage.py
@@ -8,24 +8,37 @@
 
 // COMMAND ----------
 
-// MAGIC %python
-// MAGIC spark.conf.set("fs.azure.account.key.adlstestg55.dfs.core.windows.net","969+71yPsy6O7riN0kAdTNHqWSMSRBnkLlL9oV6aPJd+btNOq4qUddrQS+4m5Id7xrzyld1P7L8K33UXxxOWqA==")
+// # %python
+// # spark.conf.set("fs.azure.account.key.adlstestg55.dfs.core.windows.net","969+71yPsy6O7riN0kAdTNHqWSMSRBnkLlL9oV6aPJd+btNOq4qUddrQS+4m5Id7xrzyld1P7L8K33UXxxOWqA==")
+
+// COMMAND ----------
+
+# %python
+#Specify the source path
+# sourcepath= spark.read.parquet("abfss://<container-name>@<storage-account-name>.dfs.core.windows.net/<directory-name>")
+
+// COMMAND ----------
+
+# %python
+# sourcepath="abfss://datalakecontainer@adlstestg55.dfs.core.windows.net/parquetdata/"
+
+// COMMAND ----------
+
+# %python
+# df=spark.read.parquet(sourcepath)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC # Secret Scope
+// MAGIC * A secret scope and key has been created in Databricks CLI
+// MAGIC * The access key of the storage account was grabbed and put in the Secret Scope
+// MAGIC * It means the secret scope being a logical container is storing all the keys of my storage account which can be levraged in my code
 
 // COMMAND ----------
 
 // MAGIC %python
-// MAGIC #Specify the source path
-// MAGIC # sourcepath= spark.read.parquet("abfss://<container-name>@<storage-account-name>.dfs.core.windows.net/<directory-name>")
-
-// COMMAND ----------
-
-// MAGIC %python
-// MAGIC sourcepath="abfss://datalakecontainer@adlstestg55.dfs.core.windows.net/parquetdata/"
-
-// COMMAND ----------
-
-// MAGIC %python
-// MAGIC df=spark.read.parquet(sourcepath)
+// MAGIC blobAccesskey = dbutils.secrets.get(scope ="myblob", key= "accesskey")
 
 // COMMAND ----------
 
