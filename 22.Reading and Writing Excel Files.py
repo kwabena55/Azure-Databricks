468f8fc4b24f40e72eac10af747fe894e2d22bb6
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-06-28 13:07:38 -0600

    Update: 00.Introduction.py 00.IntroductionII.py 02.Databases and Tables.py 03.Load Files in Databricks.py 04.Structured Streaming using Python DataFrames.py 05.2021-06-19 - JDBC Example.py 06. Data Managements Using Scala.scala 07.SQL Notebook.sql 08.DBUTILS Commands.py 09.Variables, Widgets, Notebook Parameters.py 11. Azure Databricks CLI.py 13.read CSV Files Efficiently in Spark.py 14.Read TSV Files Efficiently in Spark.py 15.Read  All PIPE Delimited CSV Files in Spark.py 16.Use Params and Read CSV Files with Multiple Delimiters.py 20.Read and Write Parquet  File in DB.py 20b.Read Parquet Files  From Datalake and Blob Storage.py 20c. Read Data from Datalake in Databricks( Big Data Analytics).py 22.Reading and Writing Excel Files.py ARM Template.json Connect by JDBC.py README.md

--- 22.Reading and Writing Excel Files.py
+++ 22.Reading and Writing Excel Files.py
@@ -1,7 +1,6 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC # Read and Write Excel Files
-# MAGIC * By default all installed libraries will be in FileStore/tables
 
 # COMMAND ----------
 
@@ -9,3 +8,27 @@ dbutils.fs.ls("FileStore/tables")
 
 # COMMAND ----------
 
+# Create an input file for Excel Files
+dbutils.widgets.text("Filename",".excel")
+
+# COMMAND ----------
+
+dbutils.widgets.get("Filename")
+
+# COMMAND ----------
+
+dbutils.fs.mkdirs("FileStore/tables/excelinput")
+
+# COMMAND ----------
+
+dbutils.fs.ls("FileStore/tables/excelinput")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Read Excel Files
+
+# COMMAND ----------
+
+com.crealytics:spark-excel_2.12:0.13.7
+    /FileStore/tables/excelinput/data__2_.xlsx'
