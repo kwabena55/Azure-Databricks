a63ee46a07e8095865af0ff76493cb111ed70aec
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-06-28 16:16:32 -0600

    Update: 00.Introduction.py 00.IntroductionII.py 02.Databases and Tables.py 03.Load Files in Databricks.py 04.Structured Streaming using Python DataFrames.py 05.2021-06-19 - JDBC Example.py 06. Data Managements Using Scala.scala 07.SQL Notebook.sql 08.DBUTILS Commands.py 09.Variables, Widgets, Notebook Parameters.py 11. Azure Databricks CLI.py 13.read CSV Files Efficiently in Spark.py 14.Read TSV Files Efficiently in Spark.py 15.Read  All PIPE Delimited CSV Files in Spark.py 16.Use Params and Read CSV Files with Multiple Delimiters.py 20.Read and Write Parquet  File in DB.py 20b.Read Parquet Files  From Datalake and Blob Storage.py 20c. Read Data from Datalake in Databricks( Big Data Analytics).py 22.Reading and Writing Excel Files.py ARM Template.json Connect by JDBC.py README.md

--- 22.Reading and Writing Excel Files.py
+++ 22.Reading and Writing Excel Files.py
@@ -47,11 +47,57 @@ data.select("country","country_code").show()
 
 # COMMAND ----------
 
-data=spark.read.format("com.crealytics.spark.excel").option("header",True).option("header",True).option("inferSchema",True).option("dataAddress","'sheet2'!").load("/FileStore/tables/excelinput/data.xlsx")
+data2=spark.read.format("com.crealytics.spark.excel").option("header",True).option("header",True).option("inferSchema",True).option("dataAddress","'sheet2'!").load("/FileStore/tables/excelinput/data.xlsx")
+
+# COMMAND ----------
+
+display(data2)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Read Specific Cells
+
+# COMMAND ----------
+
+data=spark.read.format("com.crealytics.spark.excel").option("header",True).option("header",True).option("inferSchema",True).option("dataAddress","'sheet2'!A2:C3").load("/FileStore/tables/excelinput/data.xlsx")
 
 # COMMAND ----------
 
 display(data)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Write Excel File to a Path
+
+# COMMAND ----------
+
+data.write.format("com.crealytics.spark.excel").option("header",True).save("/FileStore/tables/exceloutput/myexcel.xlsx")
+
+# COMMAND ----------
+
+data.show()
+
+# COMMAND ----------
+
+fileoutput="/FileStore/tables/exceloutput/myexcel.xlsx"
+df=spark.read.format("com.crealytics.spark.excel").option("header",True).load(fileoutput)
+
+# COMMAND ----------
+
+df.show()
+
+# COMMAND ----------
+
+fileoutput2="/FileStore/tables/exceloutput/myexcel2.xlsx"
+df=data2.write.format("com.crealytics.spark.excel").option("header",True).option("dataAddress","'solosheet'!A1:B1").save(fileoutput2)
+
+# COMMAND ----------
+
+path="/FileStore/tables/exceloutput/myexcel2.xlsx"
+data3=spark.read.format("com.crealytics.spark.excel").option("header",True).load(path)
+data3.show()
 
 # COMMAND ----------
 
