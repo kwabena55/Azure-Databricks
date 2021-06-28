f9534ac52c29684df3c8ad72565e8e420ae5dad1
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-06-28 11:58:28 -0600

    Update: 00.Introduction.py 00.IntroductionII.py 02.Databases and Tables.py 03.Load Files in Databricks.py 04.Structured Streaming using Python DataFrames.py 05.2021-06-19 - JDBC Example.py 06. Data Managements Using Scala.scala 07.SQL Notebook.sql 08.DBUTILS Commands.py 09.Variables, Widgets, Notebook Parameters.py 11. Azure Databricks CLI.py 13.read CSV Files Efficiently in Spark.py 14.Read TSV Files Efficiently in Spark.py 15.Read  All PIPE Delimited CSV Files in Spark.py 16.Use Params and Read CSV Files with Multiple Delimiters.py 20.Read and Write Parquet  File in DB.py 20b.Read Parquet Files  From Datalake and Blob Storage.py 20c. Read Data from Datalake in Databricks( Big Data Analytics).py ARM Template.json Connect by JDBC.py README.md

--- 20c. Read Data from Datalake in Databricks( Big Data Analytics).py
+++ 20c. Read Data from Datalake in Databricks( Big Data Analytics).py
@@ -1,4 +1,12 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # Create an App Registration in Azure and Give permission to Access ADLS
+# MAGIC * So basically we are creating an application to access data
+# MAGIC * Make sure your applcation will have access to files and folders in your datalake
+# MAGIC * Filesystemname variable is the same as the Datalake container name
+
+# COMMAND ----------
+
   
 val appID = ""
 val password = ""
@@ -12,13 +20,19 @@ val configs =  Map("fs.azure.account.auth.type" -> "OAuth",
        "fs.azure.account.oauth2.client.secret" -> password,
        "fs.azure.account.oauth2.client.endpoint" -> ("https://login.microsoftonline.com/" + tenantID + "/oauth2/token"),
        "fs.azure.createRemoteFileSystemDuringInitialization"-> "true")
-
-# COMMAND ----------
-
 dbutils.fs.mount(
 source = "abfss://" + fileSystemName + "@" + storageAccountName + ".dfs.core.windows.net/",
 mountPoint = "/mnt/data",
 extraConfigs = configs)
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Mount Data Lake and Attach it as A Local Drive
+
+# COMMAND ----------
+
 
 
 val df = spark.read.csv("/mnt/data/demo/movies.csv")
