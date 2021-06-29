e693fcd5afd45d1490811d5a03a73a81e281a8d5
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-06-29 12:38:07 -0600

    Update: 00.Introduction.py 00.IntroductionII.py 02.Databases and Tables.py 03.Load Files in Databricks.py 04.Structured Streaming using Python DataFrames.py 05.2021-06-19 - JDBC Example.py 06. Data Managements Using Scala.scala 07.SQL Notebook.sql 08.DBUTILS Commands.py 09.Variables, Widgets, Notebook Parameters.py 11. Azure Databricks CLI.py 13.read CSV Files Efficiently in Spark.py 14.Read TSV Files Efficiently in Spark.py 15.Read  All PIPE Delimited CSV Files in Spark.py 16.Use Params and Read CSV Files with Multiple Delimiters.py 20.Read and Write Parquet  File in DB.py 20b.Read Parquet Files  From Datalake and Blob Storage.py 20c. Read Data from Datalake in Databricks( Big Data Analytics).py 22.Reading and Writing Excel Files.py 23.Read_Write AVRO and ORC Files.py ARM Template.json Connect by JDBC.py README.md data.xml

--- 23.Read_Write AVRO and ORC Files.py
+++ 23.Read_Write AVRO and ORC Files.py
@@ -1,1 +1,41 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # Delete all COntents in a Folder
+
+# COMMAND ----------
+
+dbutils.fs.rm("FileStore/tables/", True)
+
+# COMMAND ----------
+
+dbutils.fs.mkdirs("FileStore/tables/inputavro/")
+dbutils.fs.mkdirs("FileStore/tables/outputavro/")
+
+# COMMAND ----------
+
+dbutils.fs.mkdirs("FileStore/tables/inputorc/")
+dbutils.fs.mkdirs("FileStore/tables/outputorc/")
+
+# COMMAND ----------
+
+dbutils.fs.ls("FileStore/tables/inputorc/")
+
+# COMMAND ----------
+
+dbutils.fs.ls("FileStore/tables/inputavro/")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Load/Read ORC Files
+
+# COMMAND ----------
+
+dfavro=spark.read.format("orc").option("header",True).load('dbfs:/FileStore/tables/inputorc/userdata1_orc')
+
+# COMMAND ----------
+
+display(dfavro)
+
+# COMMAND ----------
+
