2364b3b0e58c521e71d4eba4d40dbcf3538b07f7
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-07-01 13:06:00 -0600

    Update: 00.Introduction.py 00.IntroductionII.py 02.Databases and Tables.py 03.Load Files in Databricks.py 04.Structured Streaming using Python DataFrames.py 05.2021-06-19 - JDBC Example.py 06. Data Managements Using Scala.scala 07.SQL Notebook.sql 08.DBUTILS Commands.py 09.Variables, Widgets, Notebook Parameters.py 11. Azure Databricks CLI.py 13.read CSV Files Efficiently in Spark.py 14.Read TSV Files Efficiently in Spark.py 15.Read  All PIPE Delimited CSV Files in Spark.py 16.Use Params and Read CSV Files with Multiple Delimiters.py 20.Read and Write Parquet  File in DB.py 20b.Read Parquet Files  From Datalake and Blob Storage.py 20c. Read Data from Datalake in Databricks( Big Data Analytics).py 22.Reading and Writing Excel Files.py 23.Read_Write AVRO and ORC Files.py 24. Logging in Databricks( Pyspark).py ARM Template.json Connect by JDBC.py README.md data.xml userdata1.avro userdata1_orc userdata4_orc

--- 24. Logging in Databricks( Pyspark).py
+++ 24. Logging in Databricks( Pyspark).py
@@ -8,7 +8,15 @@ dbutils.notebook.help()
 # COMMAND ----------
 
 import logging
-logging .debug
+logging .debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')
+
+# COMMAND ----------
+
+
 
 # COMMAND ----------
 
