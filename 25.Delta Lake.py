771bf6caf241dfaa65cc8a635588df0f49c933c4
Solomon kwabena Ansah <44747270+kwabena55@users.noreply.github.com>
2021-07-02 14:12:31 -0600

    Update: 00.Introduction.py 00.IntroductionII.py 02.Databases and Tables.py 03.Load Files in Databricks.py 04.Structured Streaming using Python DataFrames.py 05.2021-06-19 - JDBC Example.py 06. Data Managements Using Scala.scala 07.SQL Notebook.sql 08.DBUTILS Commands.py 09.Variables, Widgets, Notebook Parameters.py 11. Azure Databricks CLI.py 13.read CSV Files Efficiently in Spark.py 14.Read TSV Files Efficiently in Spark.py 15.Read  All PIPE Delimited CSV Files in Spark.py 16.Use Params and Read CSV Files with Multiple Delimiters.py 20.Read and Write Parquet  File in DB.py 20b.Read Parquet Files  From Datalake and Blob Storage.py 20c. Read Data from Datalake in Databricks( Big Data Analytics).py 22.Reading and Writing Excel Files.py 23.Read_Write AVRO and ORC Files.py 24. Logging in Databricks( Pyspark).py 25.Delta Lake.py ARM Template.json Connect by JDBC.py README.md data.xml userdata1.avro userdata1_orc userdata4_orc

--- 25.Delta Lake.py
+++ 25.Delta Lake.py
@@ -1,1 +1,27 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # Create DataFrame
+
+# COMMAND ----------
+
+df=spark.createDataFrame([(1,"solomon","Edmonton"),(2,"louisa","calgary"),(3,"Elikem","Chapelle")],["cid","cname","clocation"])
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Write Data to DBFS
+
+# COMMAND ----------
+
+df.write.format("parquet").save("dbfs:/FileStore/tables/parquetdata/")
+
+# COMMAND ----------
+
+dbutils.fs.ls("dbfs:/FileStore/tables/parquetdata/")
+
+# COMMAND ----------
+
